9f1ce9f6761689b73ab0b21d9478fc0f95368c65
Jesse Ma <j.masonoma17@gmail.com>
2019-05-14 19:53:19 -0700

    Remove cleanspeak-related code from moderation controller

--- router/api/moderate.js
+++ router/api/moderate.js
@@ -1,14 +1,14 @@
-const ModerationCtrl = require('../../controllers/ModerationCtrl')
+// const ModerationCtrl = require('../../controllers/ModerationCtrl')
 
 module.exports = router => {
   router.route('/moderate/message').post((req, res) => {
-    ModerationCtrl.moderateMessage(req.body, (err, isClean) => {
-      if (err) {
-        console.log(err)
-        res.json({ err })
-      } else {
-        res.json({ isClean })
-      }
-    })
+    // ModerationCtrl.moderateMessage(req.body, (err, isClean) => {
+    //   if (err) {
+    //     console.log(err)
+    //     res.json({ err })
+    //   } else {
+    //     res.json({ isClean })
+    //   }
+    // })
   })
 }
